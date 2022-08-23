b198b9671426d6c7ac1d727b91bf4c802e026b09
wild-kard <43704663+wild-kard@users.noreply.github.com>
2022-08-23 13:07:46 -0400

    Update 5-fastsync.sh

--- 5-fastsync.sh
+++ 5-fastsync.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+
+# Copyright (c) 2022 Ben Westgate
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 readonly SIZE=$(df --output=size --block-size=M . | tail -1 | head -c-2)
 readonly CHAINSTATE=$(du --block-size=M /media/amnesia/a988dd30-61b1-49d7-88f4-50b8c450e5c0/.bitcoin/chainstate | cut -f1 | head -c-2)
 readonly BUFFER=$((CHAINSTATE > 6144 ? 10 * CHAINSTATE / 6 : 10240))
